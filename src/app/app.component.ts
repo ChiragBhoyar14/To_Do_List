f3fc6c330dcce82a3230aab0227c02dd439185f3
Chirag Bhoyar <149136136+ChiragBhoyar14@users.noreply.github.com>
2025-07-28 19:30:04 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './task/task.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent, TaskComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+ dummy_user = DUMMY_USERS;
+ selectedUserId: string = 'u1';
+
+  getselectedUser() {
+    return this.dummy_user.find(user=>user.id==this.selectedUserId);
+  }
+
+  onSelectedUse(id: string) {
+    this.selectedUserId = id;
+  }
+
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,31 @@
+import { Component,Input,Output, EventEmitter} from '@angular/core';
+
+type  User =  {
+  id: string;
+  name: string;
+  avatar: string;
+}
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+export class UserComponent {
+    
+   @Input({required: true}) user!: User;
+
+    @Output() select = new EventEmitter<string>();
+    
+    getUserAvtar ()
+    { 
+      return 'assets/users/' + this.user.avatar
+    }
+
+    onSelectedUser() 
+    {
+      this.select.emit(this.user.id);
+    }
+  }
